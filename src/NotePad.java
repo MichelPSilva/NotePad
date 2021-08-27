55db8e18be08069f36533dbe8fc925df4fcc9ae2
RafaelMQs <rafaeelmq08@gmail.com>
2021-08-26 22:46:17 -0300

    Adicionando Menu

--- src/NotePad.java
+++ src/NotePad.java
@@ -3,7 +3,9 @@ import java.awt.event.*;
 import javax.swing.*;
 
 public class NotePad extends JFrame{
-	private JLabel digText;
+	private JMenu arquivo;
+	private JMenuBar barMenu;
+	private JMenuItem miSair;
 	private TextArea taText;
 	private JPanel pPrinci;
 	
@@ -22,26 +24,39 @@ public class NotePad extends JFrame{
 	public void Componentes() {
 		setTitle("NotePad Crackeado");
 		setLayout(null);
-		setSize(600, 500);
+		setSize(500, 400);
 		setResizable(false);
+		
+		barMenu = new JMenuBar();
+		barMenu.setBackground(Color.white);
+		arquivo = new JMenu("Arquivo");
+		arquivo.setBackground(Color.white);
+		miSair = new JMenuItem("Sair");
+		miSair.setBackground(Color.white);
+		miSair.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_S, ActionEvent.CTRL_MASK));
+		arquivo.add(miSair);
+		barMenu.add(arquivo);
+		setJMenuBar(barMenu);
+		
 		
 		pPrinci = new JPanel(null);
 		pPrinci.setBounds(0, 0, 600, 500);
 		pPrinci.setBackground(new Color(70,130,180));
 		add(pPrinci);
 		
-		digText = new JLabel("Digite seu texto aqui: ");
-		digText.setBounds(12, 150, 150, 30);
-		pPrinci.add(digText);
-		
 		taText = new TextArea();
-		taText.setBounds(5, 180, 500, 270);
+		taText.setBounds(5, 65, 472, 270);
+		taText.setBackground(new Color (238,232,170));
 		pPrinci.add(taText);
 		
 	}
 	
 	public void Eventos() {
-		
+		miSair.addActionListener(new ActionListener() {	
+			public void actionPerformed(ActionEvent e) {
+				System.exit(0);	
+			}
+		});
 	}
 	
 }
